5a342fb56e5cac52d34d875b799fb8c933778a2c
carter2099 <carter2077@pm.me>
2026-02-22 13:29:12 -0500

    fix dollar formatting across app

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def format_usd(value, precision: 2)
+    n = number_with_delimiter(number_with_precision(value.to_f.abs, precision: precision))
+    value.to_f < 0 ? "-$#{n}" : "$#{n}"
+  end
 end
