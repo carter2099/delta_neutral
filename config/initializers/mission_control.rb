b75f86c552cbfe62996023bf4e6a7a44bf33376a
carter2099 <carter2077@pm.me>
2026-02-22 13:20:34 -0500

    change /jobs ordering

--- config/initializers/mission_control.rb
+++ config/initializers/mission_control.rb
@@ -1,2 +1,17 @@
 MissionControl::Jobs.http_basic_auth_enabled = false
 MissionControl::Jobs.base_controller_class = "ApplicationController"
+
+# Sort failed/pending/in_progress jobs by most recent first (default is oldest first)
+Rails.application.config.after_initialize do
+  ActiveJob::QueueAdapters::SolidQueueExt::SolidQueueJobs.prepend(Module.new do
+    private
+
+    def order_executions(executions)
+      if solid_queue_status.scheduled?
+        executions.ordered
+      else
+        executions.order(job_id: :desc)
+      end
+    end
+  end)
+end
